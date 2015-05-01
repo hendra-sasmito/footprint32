ed9aa73479d82ab7a003ae924268bfa692dfb1b6
hendra-sasmito <hendra_sasmito@yahoo.com>
2015-05-01 21:47:56 +0200

    remove limit

--- db/migrate/20140320195135_add_photoable_to_photos.rb
+++ db/migrate/20140320195135_add_photoable_to_photos.rb
@@ -2,7 +2,7 @@ class AddPhotoableToPhotos < ActiveRecord::Migration
   def change
 #    add_column :photos, :photoable, :references, :polymorphic => true, :null => false
 
-    add_column :photos, :photoable_id, :integer, :limit => 11
+    add_column :photos, :photoable_id, :integer
     add_column :photos, :photoable_type, :string
     add_index :photos, [:photoable_id, :photoable_type]
   end
