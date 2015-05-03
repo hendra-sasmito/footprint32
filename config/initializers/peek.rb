6c5df622ccd70362e49f467559acbc0335bee8a5
hendra-sasmito <hendra_sasmito@yahoo.com>
2015-05-03 17:54:27 +0200

    enable peek pg (postgres)

--- config/initializers/peek.rb
+++ config/initializers/peek.rb
@@ -1,4 +1,5 @@
-Peek.into Peek::Views::Mysql2
+#Peek.into Peek::Views::Mysql2
+Peek.into Peek::Views::PG
 Peek.into Peek::Views::ActiveResource
 Peek.into Peek::Views::GC
 Peek.into Peek::Views::PerformanceBar
