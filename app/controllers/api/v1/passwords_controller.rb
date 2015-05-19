fbaded27036eb486f6796566b9cac5301d1406bd
hendra-sasmito <hendra_sasmito@yahoo.com>
2015-05-19 18:06:21 +0200

    fix bug

--- app/controllers/api/v1/passwords_controller.rb
+++ app/controllers/api/v1/passwords_controller.rb
@@ -10,10 +10,10 @@ class Api::V1::PasswordsController < Devise::PasswordsController
     @user = User.find_by_email(params['user']['email'])
     if @user.present?
       @user.send_reset_password_instructions
+    end
     render :status => 200,
            :json => { :success => true,
                       :info => "Reset instruction sent" }
-    end
   end
   
 end
